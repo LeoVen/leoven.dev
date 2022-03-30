37ef8f9377b8aa7fe38cf9e47dfb6fa9dc49a3c0
Leonardo Vencovsky <leonardo.vencovsky@gmail.com>
2022-03-29 20:26:34 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='centered'>
+      <h1>Leonardo Vencovsky</h1>
+      <h2>Curriculum Vitae</h2>
+      <p>Coming soon..</p>
+    </div>
+  );
+}
+
+export default App;
