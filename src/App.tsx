7d3f07cd842bdcd2bdd88ebceaf99ee75947786c
LeoVen <leonardo.vencovsky@gmail.com>
2022-04-01 20:06:26 -0300

    Basic links added for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { AnchorButton, Button, ButtonGroup } from "@blueprintjs/core";
 
 function App() {
   return (
-    <div className='centered'>
+    <div className="centered">
       <h1>Leonardo Vencovsky</h1>
       <h2>Curriculum Vitae</h2>
-      <p>Coming soon..</p>
+      <p>Coming soon... Meanwhile, check me out here:</p>
+      <ButtonGroup minimal={true}>
+        <AnchorButton
+          text="GitHub"
+          icon="git-repo"
+          href="https://github.com/LeoVen"
+          target={"_blank"}
+          rel="noreferrer"
+        />
+        <AnchorButton
+          text="LinkedIn"
+          icon="link"
+          href="https://www.linkedin.com/in/leonardo-vencovsky-911252149/"
+          target={"_blank"}
+          rel="noreferrer"
+        />
+        <AnchorButton
+          text="CV in PDF"
+          icon="book"
+          href="./assets/files/CV_Leonardo_Vencovsky.pdf"
+          target={"_blank"}
+          rel="noreferrer"
+        />
+      </ButtonGroup>
     </div>
   );
 }
